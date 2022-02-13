dab4de21041561f874e258e180b819c4fb0311ca
Igor Miranda <igorpcmiranda@gmail.com>
2022-02-13 20:02:03 -0300

    adding the latest changes

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Header.css";
 import SearchIcon from "@mui/icons-material/Search";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 
 function Header() {
   return (
@@ -16,22 +17,27 @@ function Header() {
         alt="logo"
       ></img>
       <div className="header__search">
-        <input className="header__seacrhInput" type="text"></input>
+        <input className="header__searchInput" type="text"></input>
         <SearchIcon className="header__searchIcon" />
       </div>
 
       <div className="header__nav">
         <div className="header__option">
           <span className="header__optionLineOne">Hello guest</span>
-          <span className="header__optionLineOne">Sign in</span>
+          <span className="header__optionLineTwo">Sign in</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Returns</span>
-          <span className="header__optionLineOne">& others</span>
+          <span className="header__optionLineTwo">& others</span>
         </div>
         <div className="header__option">
           <span className="header__optionLineOne">Your</span>
-          <span className="header__optionLineOne">Prime</span>
+          <span className="header__optionLineTwo">Prime</span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingCartIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
         </div>
       </div>
     </div>
---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          alt="Echo Studio: O som mais extraordinário com Alexa"
+          src="https://m.media-amazon.com/images/I/71eJjaWlx+L._SX3000_.jpg"
+          className="home__image"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
